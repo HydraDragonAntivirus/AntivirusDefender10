80b66f850e67a2d1c5515b9fd1aef421e87e8ace
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-11-05 11:24:07 +0300

    Aggressive fix

--- AntivirusDefender10/AntivirusDefender10/Form1.vb
+++ AntivirusDefender10/AntivirusDefender10/Form1.vb
@@ -593,19 +593,6 @@ Public Class Form1
         e.Cancel = True
     End Sub
 
-    ' Override WndProc to disable Alt + F4
-    Protected Overrides Sub WndProc(ByRef m As Message)
-        Const WM_SYSCOMMAND As Integer = &H112
-        Const SC_CLOSE As Integer = &HF060
-
-        If m.Msg = WM_SYSCOMMAND AndAlso m.WParam.ToInt32() = SC_CLOSE Then
-            ' Ignore Alt + F4
-            Return
-        End If
-
-        MyBase.WndProc(m)
-    End Sub
-
     ' Override ProcessCmdKey to handle specific keys
     Protected Overrides Function ProcessCmdKey(ByRef msg As Message, keyData As Keys) As Boolean
         ' Prevent specific key combinations
