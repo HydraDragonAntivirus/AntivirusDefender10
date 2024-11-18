f8d87fa8e6ca37c099a2860ce0628a117a0ff5f7
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-11-18 15:04:08 +0300

    Revert "FullScreen"
    This reverts commit 9b5a055743df92b6f97b31ab6e98d2f1969b6b1e.

--- AntivirusDefender10/AntivirusDefender10/Form2.Designer.vb
+++ AntivirusDefender10/AntivirusDefender10/Form2.Designer.vb
@@ -57,7 +57,6 @@ Partial Class Form2
         Me.TopMost = True
         Me.ResumeLayout(False)
         Me.PerformLayout()
-        Me.Bounds = Screen.PrimaryScreen.Bounds ' Set form to full-screen
 
     End Sub
 
